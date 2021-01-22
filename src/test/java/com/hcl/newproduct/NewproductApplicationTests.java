cf92b99fa80c3002380dccc4a92c12a2d87ec3dd
Connorrichter4 <Connorrichter4@gmail.com>
2021-01-22 15:53:44 -0600

    initialized git repository

---
+++ src/test/java/com/hcl/newproduct/NewproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.newproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
